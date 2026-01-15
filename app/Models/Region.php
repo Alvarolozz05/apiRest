c606fd849027a308614e181e805f74cda25ea20e
Alvaro Lozano <alvarojuega052@gmail.com>
2026-01-15 13:26:42 +0100

    Creamos los modelos

---
+++ app/Models/Region.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Region extends Model
+{
+    protected $fillable = ['name'];
+
+    public function realms()
+    {
+        return $this->hasMany(Realm::class);
+    }
+
+    public function creatures()
+    {
+        return $this->hasMany(Creature::class);
+    }
+}
